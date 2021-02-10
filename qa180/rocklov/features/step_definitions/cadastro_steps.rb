50b3160faf81c8538ee0da9c4094a23f3b8781a6
Karina Nakamura <karina.nakamura@accurate.com.br>
2021-02-10 17:10:36 -0300

    Aula sobre conexão no banco

--- qa180/rocklov/features/step_definitions/cadastro_steps.rb
+++ qa180/rocklov/features/step_definitions/cadastro_steps.rb
@@ -1,43 +1,62 @@
 Dado('que acesso a página de cadastro') do
-    visit("http://rocklov-web:3000/signup")
+  visit("http://rocklov-web:3000/signup")
 end
 
 Quando('submeto o meu cadastro completo') do
-    find('#fullName').set 'Karina Nakamura'
-    find('#email').set Faker::Internet.free_email
-    find('#password').set '123456'
 
-    click_button('Cadastrar')
+  MongoDB.new.remove_user('ka.nakamura@hotmail.com')
+
+  find('#fullName').set 'Karina Nakamura'
+  find('#email').set 'ka.nakamura@hotmail.com'
+  find('#password').set '123456'
+
+  click_button('Cadastrar')
 end
 
 Então('sou redirecionado para o Dashboard') do
-    expect(page).to have_css ".dashboard"
+  expect(page).to have_css ".dashboard"
 end
 
-Quando('submeto o meu cadastro sem o nome') do
-  pending # Write code here that turns the phrase above into concrete actions
+Quando('submeto o meu cadastro sem o nome') do  
+  find('#email').set 'ka.nakamura@hotmail.com'
+  find('#password').set '123456'
+
+  click_button('Cadastrar')
 end
 
 Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do 
-  pending # Write code here that turns the phrase above into concrete actions
+  alert = find(".alert-dark")
+  expect(alert.text).to eql "Oops. Informe seu nome completo!"
 end
 
 Quando('submeto o meu cadastro sem o email') do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+  find('#fullName').set 'Karina Nakamura'  
+  find('#password').set '123456'
 
-Então('vejo a mensagem de alerta: Oops. Informe um email válido!') do   
-  pending # Write code here that turns the phrase above into concrete actions
+  click_button('Cadastrar')
 end
 
 Quando('submeto o meu cadastro com email incorreto') do
-  pending # Write code here that turns the phrase above into concrete actions
+  find('#fullName').set 'Karina Nakamura'
+  find('#email').set 'karina*gmail.com'
+  find('#password').set '123456'
+
+  click_button('Cadastrar')
+end
+
+Então('vejo a mensagem de alerta: Oops. Informe um email válido!') do   
+  alert = find(".alert-dark")
+  expect(alert.text).to eql "Oops. Informe um email válido!"
 end
 
 Quando('submeto o meu cadastro sem a senha') do
-  pending # Write code here that turns the phrase above into concrete actions
+  find('#fullName').set 'Karina Nakamura'
+  find('#email').set 'ka.nakamura@hotmail.com'
+  
+  click_button('Cadastrar')
 end
 
 Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do 
-  pending # Write code here that turns the phrase above into concrete actions
+  alert = find(".alert-dark")
+  expect(alert.text).to eql "Oops. Informe sua senha secreta!"
 end
